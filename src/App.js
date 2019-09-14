ae3cb0bd4e8925317ee6358aa96f254006654df1
Csépke György <csepke.gyorgy@gmail.com>
2019-09-14 16:27:30 +0200

    kickoff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    stuff: []
+  }
+  
+  render() {
+    return (
+      this.state.stuff.map(stf => {
+        return(
+          <h1 key={stf}>
+            {stf}
+          </h1>
+        )
+      })
+    );
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:60089/api/values')
+      .then(res => res.json())
+      .then((data) => {
+      this.setState({ stuff: data })})
+      .catch(console.log)
+  }
 }
 
 export default App;
