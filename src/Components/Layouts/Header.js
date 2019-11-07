2b157669f4e9c91060f968bcfcc71baa4f643d3f
Boglarka Szell <boglarka.szell@varian.com>
2019-11-07 14:53:16 +0100

    Added header navigation icons

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,6 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import { AppBar, Grid, TextField, Box } from "@material-ui/core"
+import { AppBar, Grid, TextField, Box, IconButton , Avatar, Button} from "@material-ui/core"
+import { GetProfileIconImageUrlById } from '../../DataAccessLayer';
 import SvgIcon from '@material-ui/core/SvgIcon';
+import Settings from '@material-ui/icons/Settings';
+import Comment from '@material-ui/icons/Comment'
 import '../../index.css'
 
 
@@ -9,7 +12,7 @@ class Header extends Component {
         return (
             <Fragment>
                 <AppBar position="static">
-                    <Grid container align="center" style={{height:100}} alignItems="center">
+                    <Grid container align="center" style={{ height: 100 }} alignItems="center">
                         <Grid item sm>
                             <Box height={100}>
                                 <SvgIcon onClick={this.props.redirectToUserFeed}>
@@ -29,15 +32,16 @@ class Header extends Component {
                             {this.props.loggedInUser &&
                                 <Fragment>
                                     <Box height={100}>
-                                        <SvgIcon>
-                                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-                                        </SvgIcon>
-                                        <SvgIcon>
-                                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-                                        </SvgIcon>
-                                        <SvgIcon onClick={this.props.redirectToUserPage}>
-                                            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-                                        </SvgIcon>
+                                        <IconButton aria-label="settings">
+                                            <Comment />
+                                        </IconButton>
+                                        <IconButton aria-label="settings">
+                                            <Settings />
+                                        </IconButton>
+                                        <Button>
+                                        <Avatar src={GetProfileIconImageUrlById(this.props.loggedInUser.posterProfileIconId)}  onClick={this.props.redirectToUserPage} />
+
+                                        </Button>
                                     </Box>
                                 </Fragment>
                             }
